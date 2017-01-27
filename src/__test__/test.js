7946661d699f6b9066ab0aaa34d785a80734094f
aruntk <aruntknambiar@gmail.com>
2017-01-27 15:31:32 +0530

    espree

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -3,8 +3,8 @@ import docMap from '..';
 
 describe('basics', function () {
   it('returns comments', function () {
-    const commentsAST = docMap('var x = 42; // answer');
-    const comments = commentsAST.map(function(c) {
+    const ast = docMap('var x = 42; // answer');
+    const comments = ast.comments.map(function(c) {
       return c ? c.value : '';
     }).join('\n');
     assert.equal('answer', comments.trim());
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import { parse } from 'babylon';
+import { parse } from 'espree';
 
-function docMap(js) {
-  const ast = parse(js);
-  return ast.comments;
+export const parserOptions = {
+    comment: true,
+    ecmaFeatures: {
+        experimentalObjectRestSpread: true,
+        globalReturn: true,
+        impliedStrict: true,
+        jsx: true
+    },
+    ecmaVersion: 7,
+    loc: true,
+    range: true,
+    sourceType: 'module',
+    tokens: true
+};
+
+export default function docMap(source, filename) {
+    let ast;
+
+    try {
+        ast = parse(source, parserOptions);
+    }
+    catch (e) {
+        console.error('Unable to parse %s: %s', filename, e.message);
+    }
+
+    return ast;
 }
-
-export default docMap;
